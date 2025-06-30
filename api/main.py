c332e459d2b056e8820d0e3b70733cc0faa6c419
Taha Ibourk-El-Idrissi <tahaidrissi0525@gmail.com>
2025-06-30 17:42:41 +0100

    Fix

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 import os
 
-from api.models import Task
+from api.models import Task  # Assure-toi que c'est bien le bon chemin
 from api.routes.tasks import router as task_router
 
 app = FastAPI()
@@ -13,7 +13,6 @@ async def init_db():
     mongo_uri = os.environ.get("MONGO_URI")
     if not mongo_uri:
         raise RuntimeError("MONGO_URI n'est pas défini dans les variables d'environnement")
-
     client = AsyncIOMotorClient(mongo_uri)
     db = client.get_default_database()
     await init_beanie(database=db, document_models=[Task])
