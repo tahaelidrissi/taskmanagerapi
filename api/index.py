2d85e0697d466f3a086fdfbab583c4418f2c0072
Taha Ibourk-El-Idrissi <tahaidrissi0525@gmail.com>
2025-06-30 16:02:52 +0100

    Fix abspath error in index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath())))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
 from mangum import Mangum
