597e759228d1bab179886cf48104ee248d8b7255
Taha Ibourk-El-Idrissi <tahaidrissi0525@gmail.com>
2025-06-30 17:37:43 +0100

    Fix

--- api/database.py
+++ api/database.py
@@ -8,7 +8,7 @@ load_dotenv()  # charge les variables d’environnement du fichier .env
 
 async def init_db():
     # Connexion à MongoDB
-    client = AsyncIOMotorClient(os.getenv("MONGODB_URI"))
+    client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
 
     # Initialisation de Beanie avec le modèle Task
     await init_beanie(database=client.get_default_database(), document_models=[Task])
