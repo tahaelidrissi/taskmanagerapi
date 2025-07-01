f7b4fa3c6a058839860ad7511a73d9b1cb2a25c8
Taha Ibourk-El-Idrissi <tahaidrissi0525@gmail.com>
2025-07-01 02:12:37 +0100

    Fix

--- api/main.py
+++ api/main.py
@@ -12,21 +12,32 @@ nest_asyncio.apply()
 app = FastAPI()
 
 # Variables globales pour cache de connexion
+is_initialized = False
 client = None
 db = None
-is_initialized = False
 
 async def init_db_once():
     global client, db, is_initialized
     if not is_initialized:
+        import nest_asyncio
+        nest_asyncio.apply()
+
+        import os
+        from motor.motor_asyncio import AsyncIOMotorClient
+        from beanie import init_beanie
+        from api.models import Task
+
         mongo_uri = os.environ.get("MONGO_URI")
         if not mongo_uri:
-            raise RuntimeError("MONGO_URI n'est pas défini dans les variables d'environnement")
+            raise RuntimeError("MONGO_URI n'est pas défini")
+
         client = AsyncIOMotorClient(mongo_uri)
         db = client.get_default_database()
+
         await init_beanie(database=db, document_models=[Task])
         is_initialized = True
         print("✅ Beanie initialisé")
+
 
 @app.get("/health")
 async def health_check():
